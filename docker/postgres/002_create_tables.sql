b5c3aafc8602386e8ec3dee0a43b2d9789f4eb14
Seun Bamidele <oluwabukolaoluwaseun@gmail.com>
2026-02-04 14:42:52 +0100

    new update

---
+++ docker/postgres/002_create_tables.sql
@@ -0,0 +1,63 @@
+-- USERS TABLE
+
+CREATE TABLE IF NOT EXISTS users(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    full_name VARCHAR(255) NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    phone VARCHAR(255) NULL,
+    password TEXT NOT NULL,
+    role VARCHAR(20) DEFAULT 'user',
+    is_active BOOLEAN DEFAULT TRUE,
+    create_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+);
+
+
+--PRODUCTS TABLE
+
+CREATE TABLE IF NOT EXISTS products(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    price NUMERIC(10,2) NOT NULL,
+    stock INT NOT NULL DEFAULT 0,
+    is_active BOOLEAN DEFAULT TRUE,
+    create_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+);
+
+-- ORDERS TABLE
+
+CREATE TABLE IF NOT EXISTS orders(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID NOT NULL,
+    status VARCHAR(20) DEFAULT 'pending',
+    total_amount NUMERIC(10,2) NOT NULL,
+    create_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW(),
+
+    CONSTRAINT fk_orders_user
+        FOREIGN KEY (user_id)
+        REFERENCES users(id)
+        ON DELETE CASCADE
+);
+
+
+--ORDER ITEMS TABLE
+
+CREATE TABLE IF NOT EXISTS order_items(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    order_id UUID NOT NULL,
+    product_id UUID NOT NULL,
+    quantity INT NOT NULL CHECK (quantity > 0),
+    price NUMERIC(10,2) NOT NULL,
+
+    CONSTRAINT fk_items_order
+        FOREIGN KEY (order_id)
+        REFERENCES orders(id)
+        ON DELETE CASCADE,
+
+    CONSTRAINT fk_items_product
+        FOREIGN KEY (product_id)
+        REFERENCES products(id)
+)
---
+++ docker/postgres/003_indexes.sql
@@ -0,0 +1,3 @@
+CREATE INDEX IF NOT EXISTS idx_users_email ON users(email);
+CREATE INDEX IF NOT EXISTS idx_orders_user_id ON orders(user_id);
+CREATE INDEX IF NOT EXISTS idx_order_items_order_id ON order_items(order_id);
